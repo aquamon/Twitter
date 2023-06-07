1b203ad5f73b2d4981b4a7a454d5029ca2a331b2
Monash Chhetri <monash.chhetri22@gmail.com>
2023-06-07 14:23:40 +0530

    Adding comment

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const connect = require('./config/database');
 
-const Tweet = require('./models/tweet')
+const TweetRepository = require('./repository/tweet-repository')
 
 const app = express();
 
@@ -14,11 +14,38 @@ app.listen(PORT, async ()=>{
 
     console.log('Mongo Db Connected');
 
-    const tweet = await Tweet.create({
-        content : 'First Tweet',
-        userEmail : 'a@b.com'
-    });
+    // const tweet = await Tweet.create({
+    //     content : 'Third Tweet',
+    //     userEmail : 'a@b.com'
+    // });
 
+    // const tweet = await Tweet.find({userEmail : 'a@b.com'});
+
+    // const tweet = await Tweet.findById('647856e0a44964b67ca29684');
+
+    // tweet.userEmail = 'asssb@c.com';
+
+    // tweet.save();
+
+    // console.log(typeof(tweet));
+
+    const tweetRepo = new TweetRepository();
+
+    // const tweet = await tweetRepo.get('647857483b95dc3823488ea1');
+    
+    // const tweet = await tweetRepo.update('647857483b95dc3823488ea1',{content : 'New latest content here'});
+
+    // const tweet = await tweetRepo.update('647857483b95dc3823488ea1',{content : 'Now Ill be returned aasssaqs'});
+
+    const tweet = await tweetRepo.create({content : 'Tweet with comments'});
     console.log(tweet);
+    tweet.comments.push({content : 'First Comment'});
+    tweet.save();
+    console.log(tweet);
+
+
+
+
+
 
 }); 
--- src/models/tweet.js
+++ src/models/tweet.js
@@ -8,9 +8,18 @@ const tweetSchema = new mongoose.Schema({
     },
     userEmail : {
         type : String
-    }
+    },
 
-})
+    comments : [
+        {
+            content : {
+                type : String,
+                required : true
+            }
+        }
+    ]
+
+} , {timestamps  :true});
 
 const Tweet = mongoose.model('Tweet' , tweetSchema);
 
